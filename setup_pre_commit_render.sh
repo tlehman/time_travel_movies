f246c945ebf36b163c8ea43789f215ab63929f92
Tobi Lehman <tobi.lehman@gmail.com>
2013-04-13 22:49:32 -0700

    Add more movies

--- setup_pre_commit_render.sh
+++ setup_pre_commit_render.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
 mv .git/hooks/pre-commit.sample .git/hooks/pre-commit
-echo <<EOF
-#!/bin/sh
-dot time.gv -Tpng > time.png
-git add time.png
-EOF > .git/hooks/pre-commit
+echo "#!/bin/sh
+dot time.gv -Tpng > time.png && git add time.png" > .git/hooks/pre-commit
